32327fec21b5b0faba93b4c0538f7932efa4a443
inexistentHeap404 <sanjayv0521@gmail.com>
2025-06-22 00:15:59 +0530

    goat

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/webhook', (req, res) => {
   const mode = req.query['hub.mode'];
   const token = req.query['hub.verify_token'];
   const challenge = req.query['hub.challenge'];
-  if (mode === 'subscribe' && token === VERIFY_TOKEN) res.status(200).send(challenge);
+  if (mode && token === VERIFY_TOKEN) res.status(200).send(challenge);
   else res.sendStatus(403);
 });
 
