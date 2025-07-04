2b23f64cc004e2c358b15ed4cea33ea316aecdb2
inexistentHeap404 <sanjayv0521@gmail.com>
2025-07-04 23:16:43 +0530

    array approach

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ const app = express();
 app.use(express.json());
 
 const VERIFY_TOKEN = 'lol';
-// THE BELOW PAGE ACCESS TOKEN IS VALID FOR 60 DAYS
 const PAGE_ACCESS_TOKEN = 'EAAJZBKaZASHrABOzhG1KWyx6al3yVbZBsXiv4oEWKVtjrG5hR4T1txEAvhyRFeMxaxLvIjGikEavlBxXTq8cZB0WoDI8t0nSbUEWU2cs2Ppb9mXiIIzouNVr4y8xHhqqOxemUoqW6hh6rvNlwJZBL6Sx86CUewZAaL2jQSDj4vk6yuIZC0rE2v1anX0LQB6ZBhdtaZCBj';
 const IG_USER_ID = '700137003181494';
+
 app.get('/webhook', (req, res) => {
   const mode = req.query['hub.mode'];
   const token = req.query['hub.verify_token'];
@@ -21,16 +21,22 @@ app.get('/webhook', (req, res) => {
   }
 });
 
-/*app.post('/webhook', async (req, res) => {
+const handledCommentIds = [];
+
+app.post('/webhook', async (req, res) => {
   const entry = req.body.entry?.[0];
   const changes = entry?.changes?.[0];
-  let lastCommentId;
+
   if (changes?.field === 'comments') {
     const comment = changes.value;
     const senderId = comment.from.id;
     const commentId = comment.id;
+
+    if (handledCommentIds.includes(commentId)) return res.sendStatus(200);
+    handledCommentIds.push(commentId);
+
     try {
-      try{
+      try {
         await axios.post(
           `https://graph.facebook.com/v20.0/${comment.id}/replies`,
           {
@@ -39,14 +45,14 @@ app.get('/webhook', (req, res) => {
           {
             headers: {
               Authorization: `Bearer ${PAGE_ACCESS_TOKEN}`,
-              'Content-Type': 'application/json',
+              'Content-Type': 'application/json'
             }
           }
         );
-      }
-      catch(err){
+      } catch (err) {
         console.error(err.response?.data || err.message);
       }
+
       await axios.post(
         `https://graph.facebook.com/v23.0/${IG_USER_ID}/messages`,
         {
@@ -64,8 +70,40 @@ app.get('/webhook', (req, res) => {
       console.error(err.response?.data || err.message);
     }
   }
+
   res.sendStatus(200);
-});*/
+});
+
+app.listen(3000, () => {
+  console.log('http://localhost:3000');
+});
+
+
+
+
+/*
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
+const app = express();
+app.use(express.json());
+
+const VERIFY_TOKEN = 'lol';
+// THE BELOW PAGE ACCESS TOKEN IS VALID FOR 60 DAYS
+const PAGE_ACCESS_TOKEN = 'EAAJZBKaZASHrABOzhG1KWyx6al3yVbZBsXiv4oEWKVtjrG5hR4T1txEAvhyRFeMxaxLvIjGikEavlBxXTq8cZB0WoDI8t0nSbUEWU2cs2Ppb9mXiIIzouNVr4y8xHhqqOxemUoqW6hh6rvNlwJZBL6Sx86CUewZAaL2jQSDj4vk6yuIZC0rE2v1anX0LQB6ZBhdtaZCBj';
+const IG_USER_ID = '700137003181494';
+app.get('/webhook', (req, res) => {
+  const mode = req.query['hub.mode'];
+  const token = req.query['hub.verify_token'];
+  const challenge = req.query['hub.challenge'];
+
+  if (mode && token === VERIFY_TOKEN) {
+    res.status(200).send(challenge);
+  } else {
+    res.sendStatus(403);
+  }
+});
 
 let lastCommentId;
 
@@ -126,3 +164,5 @@ app.post('/webhook', async (req, res) => {
 app.listen(3000, () => {
   console.log('http://localhost:3000');
 });
+
+*/
